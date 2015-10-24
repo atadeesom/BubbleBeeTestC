460a5ffc545cadeb18af258ce6d5d1a09fe4dc9a
Mimmpk <mimmii.parichat@gmail.com>
2015-10-24 19:19:40 +0700

    Add Source Code

---
+++ DemoCalculator/AdvanceCalculator/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceCalculator
+{
+    public class Calculator
+    {
+        public int operatorPlus(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int operatorMinus(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int operatorMultiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int operatorDivide(int a, int b)
+        {
+            return a / b;
+        }
+
+    }
+}
---
+++ DemoCalculator/CalculatorUnitTest/CalculatorTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleCalculator;
+
+namespace CalculatorUnitTest
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        private int a;
+        private int b;
+
+        [TestMethod]
+        public void operatorPlusTest()
+        {
+            Calculator calculator = new Calculator();
+            // Input Parameter
+            a = 3;
+            b = 4;
+            int expectedResult = 7;
+            Assert.AreEqual(expectedResult, calculator.operatorPlus(a,b), "Expected Result is 7 but Actual Result is {0}", expectedResult);
+        }
+
+        [TestMethod]
+        public void operatorMinusTest()
+        {
+            Calculator calculator = new Calculator();
+            // Input Parameter
+            a = 5;
+            b = 4;
+            int expectedResult = 1;
+            Assert.AreEqual(expectedResult, calculator.operatorMinus(a, b), "Expected Result is 1 but Actual Result is {0}", expectedResult);
+        }
+
+        [TestMethod]
+        public void operatorMultiplyTest()
+        {
+            Calculator calculator = new Calculator();
+            // Input Parameter
+            a = 10;
+            b = 5;
+            int expectedResult = 50;
+            Assert.AreEqual(expectedResult, calculator.operatorMultiply(a, b), "Expected Result is 50 but Actual Result is {0}", expectedResult);
+        }
+
+        [TestMethod]
+        public void operatorDivideTest()
+        {
+            Calculator calculator = new Calculator();
+            // Input Parameter
+            a = 10;
+            b = 5;
+            int expectedResult = 2;
+            Assert.AreEqual(expectedResult, calculator.operatorDivide(a, b), "Expected Result is 2 but Actual Result is {0}", expectedResult);
+        }
+
+    }
+}
